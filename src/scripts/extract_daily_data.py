52238550bc8d3ddad0a25835c8b2a85eb666f6a5
mglevitt <59936512+mglevitt@users.noreply.github.com>
2021-11-05 15:38:11 -0700

    data formatted

--- src/scripts/extract_daily_data.py
+++ src/scripts/extract_daily_data.py
@@ -26,7 +26,51 @@ for t in teleconnections:
     df = convert_txt_to_csv(t)
     dfs.append(df)
 combined_df = pd.concat(dfs)
-print(combined_df.head())
-cwd = os.getcwd()
-file_path = f'{cwd}/src/data/daily_data.csv'
-combined_df.to_csv(file_path)
+dates = ['year','month','day']
+for col in dates:
+    combined_df[col] = combined_df[col].astype(int)
+#print(combined_df.head())
+#cwd = os.getcwd()
+#file_path = f'{cwd}/src/data/daily_data.csv'
+#combined_df.to_csv(file_path)
+sorted_df = combined_df.sort_values(by = dates)
+
+
+
+def extract_column_data(tel,start):
+    c_list = []
+    def extract_data_for_year(year,start):
+        cnt = 0
+        dec_1_in = df[(df['year'] == year) & (df['month'] == 12) & (df['day'] == 1)].index[0]
+        c_in = dec_1_in + start
+        while cnt < 90:
+            c_list.append(df.loc[c_in]['level'])
+            c_in += 1
+            cnt += 1
+    df = sorted_df[sorted_df['tel']==tel]
+    for year in range(1950,2011):
+        extract_data_for_year(year,start)
+    return c_list
+
+def create_csv(offset):
+    data = {}
+    if offset == 2:
+        start = -22
+        end = 17
+    else:
+        start = -15 - (offset * 3)
+        end = 16
+    for tel in teleconnections:
+        for i in range(start,end,offset):
+            feature_data = extract_column_data(tel,i)
+            feature_name = f'{tel}{str(i)}'
+            data[feature_name] = feature_data
+    df = pd.DataFrame(data)
+    cwd = os.getcwd()
+    file_name = f'temporal_offset_{str(offset)}_data.csv'
+    file_path = f'{cwd}/src/data//{file_name}'
+    df.to_csv(file_path)
+
+for i in range(1,4):
+    create_csv(i)
+
--- src/scripts/extract_monthly_data.py
+++ src/scripts/extract_monthly_data.py
@@ -36,6 +36,7 @@ combined_df = pd.concat(dfs)
 print(combined_df.head())
 cwd = os.getcwd()
 file_path = f'{cwd}/src/data/monthly_data.csv'
+djf_file_path = f'{cwd}/src/data/monthly_djf_data.csv'
 combined_df.to_csv(file_path)
 
 enso_df = combined_df[combined_df['tel'] == 'enso']
@@ -47,6 +48,11 @@ df = pd.read_csv(f'{cwd}/src/data/daily_data.csv')
 grouped_df = df.groupby(['year','month','tel']).mean()
 monthly_df = grouped_df.reset_index().drop(columns = ['Unnamed: 0','day'])
 monthly_df = pd.concat([monthly_df,enso_df])
+djf_df = monthly_df.loc[monthly_df['month'].isin([12,1,2])]
 print(monthly_df.head())
+print(djf_df.head())
+
 
 monthly_df.to_csv(file_path)
+djf_df.to_csv(djf_file_path)
+
