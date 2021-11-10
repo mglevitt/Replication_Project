a8898451242be7850ae957a88cfa3d3fa57e6773
mglevitt <59936512+mglevitt@users.noreply.github.com>
2021-11-09 21:48:27 -0800

    added logging

--- src/scripts/extract_daily_data.py
+++ src/scripts/extract_daily_data.py
@@ -3,74 +3,99 @@ import csv
 import regex as re
 import os
 from pathlib import Path
+import logging
 
-def convert_txt_to_csv(tel):
-    file_name = f'{tel}.reanalysis.t10trunc.1948-present.txt'
-    cwd = os.getcwd()
-    file_path = f'{cwd}/src/data/teleconn/{file_name}'
-    with open(file_path,'r') as f:
-        lines = f.readlines()
-    data = []
-    pat = '[\d\.]+'
-    for line in lines:
-        t = re.findall(pat,line)
-        data.append(t)
-    df = pd.DataFrame(data)
-    df = df.rename(columns={0:'year',1:'month',2:'day',3:'level'})
-    df['tel'] = tel
-    return df
 
-teleconnections = ['epo','nao','pna','wpo']
-dfs = []
-for t in teleconnections:
-    df = convert_txt_to_csv(t)
-    dfs.append(df)
-combined_df = pd.concat(dfs)
-dates = ['year','month','day']
-for col in dates:
-    combined_df[col] = combined_df[col].astype(int)
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+logging.warning('This message will get logged on to a file')
+
+def convert_txt_to_df(tel):
+    try:
+        file_name = f'{tel}.reanalysis.t10trunc.1948-present.txt'
+        cwd = os.getcwd()
+        file_path = f'{cwd}/src/data/teleconn/{file_name}'
+        with open(file_path,'r') as f:
+            lines = f.readlines()
+        data = []
+        pat = '[\d\.]+'
+        for line in lines:
+            t = re.findall(pat,line)
+            data.append(t)
+        df = pd.DataFrame(data)
+        df = df.rename(columns={0:'year',1:'month',2:'day',3:'level'})
+        df['tel'] = tel
+        logging.info(f'The txt file for {tel} was successfully converted into a df.')
+        return df
+    except:
+        logging.error(f'Unexpected error reached while converting the txt file for {tel} into a df.')
+
+def create_tel_df():
+    try:
+        teleconnections = ['epo','nao','pna','wpo']
+        dfs = []
+        for t in teleconnections:
+            df = convert_txt_to_df(t)
+            dfs.append(df)
+        combined_df = pd.concat(dfs)
+        dates = ['year','month','day']
+        for col in dates:
+            combined_df[col] = combined_df[col].astype(int)
+        sorted_df = combined_df.sort_values(by = dates)
+        logging.info('Successfully created df with all teloconection monthly values.')
+        return sorted_df
+    except:
+        logging.error('Unexpected error reached while creating df of all teleconnections.')
 #print(combined_df.head())
 #cwd = os.getcwd()
 #file_path = f'{cwd}/src/data/daily_data.csv'
 #combined_df.to_csv(file_path)
-sorted_df = combined_df.sort_values(by = dates)
 
+sorted_df = create_tel_df()
 
 
 def extract_column_data(tel,start):
-    c_list = []
-    def extract_data_for_year(year,start):
-        cnt = 0
-        dec_1_in = df[(df['year'] == year) & (df['month'] == 12) & (df['day'] == 1)].index[0]
-        c_in = dec_1_in + start
-        while cnt < 90:
-            c_list.append(df.loc[c_in]['level'])
-            c_in += 1
-            cnt += 1
-    df = sorted_df[sorted_df['tel']==tel]
-    for year in range(1950,2011):
-        extract_data_for_year(year,start)
-    return c_list
+    try:
+        c_list = []
+        def extract_data_for_year(year,start):
+            cnt = 0
+            dec_1_in = df[(df['year'] == year) & (df['month'] == 12) & (df['day'] == 1)].index[0]
+            c_in = dec_1_in + start
+            while cnt < 90:
+                c_list.append(df.loc[c_in]['level'])
+                c_in += 1
+                cnt += 1
+        df = sorted_df[sorted_df['tel']==tel]
+        for year in range(1950,2011):
+            extract_data_for_year(year,start)
+        return c_list
+    except:
+        logging.error(f'Unexpected error geting column data for {tel} at start of {start}.')
 
 def create_csv(offset):
-    data = {}
-    if offset == 2:
-        start = -22
-        end = 17
-    else:
-        start = -15 - (offset * 3)
-        end = 16
-    for tel in teleconnections:
-        for i in range(start,end,offset):
-            feature_data = extract_column_data(tel,i)
-            feature_name = f'{tel}{str(i)}'
-            data[feature_name] = feature_data
-    df = pd.DataFrame(data)
-    cwd = os.getcwd()
-    file_name = f'temporal_offset_{str(offset)}_data.csv'
-    file_path = f'{cwd}/src/final_data/{file_name}'
-    df.to_csv(file_path)
+    try:
+        data = {}
+        if offset == 2:
+            start = -22
+            end = 17
+        else:
+            start = -15 - (offset * 3)
+            end = 16
+        teleconnections = ['epo','nao','pna','wpo']
+        for tel in teleconnections:
+            for i in range(start,end,offset):
+                feature_data = extract_column_data(tel,i)
+                feature_name = f'{tel}{str(i)}'
+                data[feature_name] = feature_data
+        df = pd.DataFrame(data)
+        cwd = os.getcwd()
+        file_name = f'temporal_offset_{str(offset)}_data.csv'
+        file_path = f'{cwd}/src/final_data/{file_name}'
+        df.to_csv(file_path)
+        logging.info(f'Successfully made {file_name}')
+    except:
+        logging.error(f'Unexpected error making csv file for data with offset of {offset}.')
 
 for i in range(1,4):
-    create_csv(i)
+    #create_csv(i)
+    logging.info(i)
 
--- src/scripts/extract_monthly_data.py
+++ src/scripts/extract_monthly_data.py
@@ -3,79 +3,132 @@ import csv
 import regex as re
 import os
 from pathlib import Path
+import logging
 
 
-def convert_txt_to_csv(tel):
-    file_name = f'{tel}_monthly_avgs.txt'
-    cwd = os.getcwd()
-    file_path = f'{cwd}/src/data/monthly/{file_name}'
-    with open(file_path,'r') as f:
-        lines = f.readlines()
-    data = []
-    pat = '[\d\.]+'
-    cnt = 1
-    for line in lines:
-        if cnt == 13:
-            cnt = 1
-        year = line[:5]
-        t = [year,cnt]
-        cnt +=1
-        lev = re.findall(pat,line[5:])
-        t.append(lev[-1])
-        data.append(t)
-    df = pd.DataFrame(data)
-    df = df.rename(columns={0:'year',1:'month',2:'level'})
-    df['tel'] = tel
-    return df
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+logging.warning('This message will get logged on to a file')
 
 teleconnections = ['epo','nao','pna','wpo','enso']
-dfs = []
-for tl in teleconnections:
-    df = convert_txt_to_csv(tl)
-    dfs.append(df)
-combined_df = pd.concat(dfs)
-#print(combined_df.head())
-cwd = os.getcwd()
-file_path = f'{cwd}/src/data/monthly_data.csv'
-djf_file_path = f'{cwd}/src/final_data/monthly_djf_data.csv'
-combined_df.to_csv(file_path)
 
-enso_df = combined_df[combined_df['tel'] == 'enso']
-
-cwd = os.getcwd()
-file_path = f'{cwd}/src/data/averaged_monthly_data.csv'
-
-df = pd.read_csv(f'{cwd}/src/data/daily_data.csv')
-grouped_df = df.groupby(['year','month','tel']).mean()
-monthly_df = grouped_df.reset_index().drop(columns = ['Unnamed: 0','day'])
-monthly_df = pd.concat([monthly_df,enso_df])
-def convert_to_int(x):
+def convert_txt_to_df(tel):
     try:
-        x = x.replace(' ','')
-        return int(x)
+        file_name = f'{tel}_monthly_avgs.txt'
+        cwd = os.getcwd()
+        file_path = f'{cwd}/src/data/monthly/{file_name}'
+        with open(file_path,'r') as f:
+            lines = f.readlines()
+        data = []
+        pat = '[\d\.]+'
+        cnt = 1
+        for line in lines:
+            if cnt == 13:
+                cnt = 1
+            year = line[:5]
+            t = [year,cnt]
+            cnt +=1
+            lev = re.findall(pat,line[5:])
+            t.append(lev[-1])
+            data.append(t)
+        df = pd.DataFrame(data)
+        df = df.rename(columns={0:'year',1:'month',2:'level'})
+        df['tel'] = tel
+        logging.info(f'Successfully made df for {tel}.')
+        return df
     except:
-        return int(x)
-monthly_df['year'] = monthly_df[['year']].applymap(convert_to_int)
-monthly_df = monthly_df[(monthly_df['year'] >= 1950)  & (monthly_df['year'] <= 2012)]
-#print(monthly_df.shape)
-djf_df = monthly_df.loc[monthly_df['month'].isin([12,1,2])]
-#print(monthly_df.head())
-#print(djf_df.head())
+        logging.error('Unexpected error making df for {tel}.')
 
-def extract_date(x):
-    x['date'] =  str(x['year']) + ' ,' + str(x['month'])
-    return x
+def create_combined_monthly_df():
+    try:
+        
+        dfs = []
+        for tl in teleconnections:
+            df = convert_txt_to_df(tl)
+            dfs.append(df)
+        combined_df = pd.concat(dfs)
+        #print(combined_df.head())
+        enso_df = combined_df[combined_df['tel'] == 'enso']
 
-djf_df = djf_df.apply(extract_date, axis = 1)
-djf_df = djf_df.sort_values(by = ['date'])
-data = {}
-for tel in teleconnections:
-    data[tel] = djf_df[djf_df['tel'] == tel]['level'].tolist()
-new_djf_df = pd.DataFrame(data)
-new_djf_df['date'] = djf_df[djf_df['tel'] == 'enso']['date'].tolist()
-new_djf_df = new_djf_df.set_index('date')
-#new_djf_df = djf_df.pivot_table(values = 'level', index = 'date',columns = 'tel')
-#print(new_djf_df.head())
-monthly_df.to_csv(file_path)
-new_djf_df.to_csv(djf_file_path)
+        cwd = os.getcwd()
+        daily_file_path = f'{cwd}/src/data/daily_data.csv'
+
+        df = pd.read_csv(daily_file_path)
+        grouped_df = df.groupby(['year','month','tel']).mean()
+        monthly_df = grouped_df.reset_index().drop(columns = ['Unnamed: 0','day'])
+        monthly_df = pd.concat([monthly_df,enso_df])
+        logging.info('Successfully created monthly data df.')
+        return monthly_df
+    except:
+        logging.error('Unexpected error reached creating monthly df.')
+
+
+def clean_monthly_df(df):
+    def convert_to_int(x):
+        try:
+            x = x.replace(' ','')
+            return int(x)
+        except:
+            return int(x)
+    try:
+        df['year'] = df[['year']].applymap(convert_to_int)
+        df = df[(df['year'] >= 1950)  & (df['year'] <= 2012)]
+        logging.info('Successfully cleaned df.')
+        return df
+    except:
+        logging.error('Unexpected error reached cleaning df.')
+
+def clean_djf_df(df):
+    def extract_date(x):
+        try:
+            x['date'] =  str(x['year']) + ' ,' + str(x['month'])
+            return x
+        except:
+            logging.error('Unexpected error creating new date value.')
+    try:
+        df = df.loc[df['month'].isin([12,1,2])]
+        df = df.apply(extract_date, axis = 1)
+        df = df.sort_values(by = ['date'])
+        logging.info('Successfully changed monthly df into djf df.')
+        return df
+    except:
+        logging.error('Unexpected error reached changing monthly df into djf df.')
+
+def create_final_djf_df(df):
+    try:
+        data = {}
+        for tel in teleconnections:
+            data[tel] = df[df['tel'] == tel]['level'].tolist()
+        new_djf_df = pd.DataFrame(data)
+        new_djf_df['date'] = df[df['tel'] == 'enso']['date'].tolist()
+        new_djf_df = new_djf_df.set_index('date')
+        logging.info('Successfully created final djf df.')
+        return new_djf_df
+    except:
+        logging.error('Unexpected error reached creating final djf df.')
+
+
+def create_monthly_csvs():
+    try:
+        monthly_df = create_combined_monthly_df()
+        c_monthly_df = clean_monthly_df(monthly_df)
+        djf_df = clean_djf_df(c_monthly_df)
+        f_djf_df = create_final_djf_df(djf_df)
+
+        cwd = os.getcwd()
+        file_path = f'{cwd}/src/data/monthly_data.csv'
+        djf_file_path = f'{cwd}/src/data/monthly_djf_data.csv'
+        f_djf_file_path = f'{cwd}/src/final_data/monthly_djf_data.csv'
+
+        c_monthly_df.to_csv(file_path)
+        djf_df.to_csv(djf_file_path)
+        f_djf_df.to_csv(f_djf_file_path)
+        logging.info('Successfully created all monthly csvs.')
+    except:
+        logging.error('Unexpected error reached creating all monthly csvs.')
+
+create_monthly_csvs()
+
+
+
+
 
